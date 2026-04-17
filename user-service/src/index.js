bb69e176b46abf035eeec7d0f696d626e94a6ba7
Emre Haksever <github-emre.haksever2@gmail.com>
2026-04-17 12:37:25 +0300

    proje iskeleti oluşturuldu.

---
+++ user-service/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json()); // Gelen JSON verilerini okuyabilmek için
+
+const PORT = process.env.PORT || 3001;
+const MONGO_URI = process.env.MONGO_URI;
+
+// MongoDB Bağlantısı
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('✅ User Service: MongoDB bağlantısı başarılı.'))
+  .catch((err) => console.error('❌ User Service: MongoDB bağlantı hatası:', err));
+
+// Sağlık Kontrolü (Healthcheck) Endpoint'i - Monitoring için lazım olacak!
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP', service: 'user-service' });
+});
+
+// Sunucuyu Başlat
+app.listen(PORT, () => {
+  console.log(`🚀 User Service ${PORT} portunda çalışıyor.`);
+});
